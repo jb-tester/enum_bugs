462e243223659eb8c0197a6ae4795585cf464e18
irina <irina@petrovskaya.me>
2015-06-10 02:19:11 +0300

    bug

--- src/a/b/c/Bar.java
+++ src/a/b/c/Bar.java
@@ -3,7 +3,7 @@ package a.b.c;
 /**
  * Created by irina on 6/10/2015.
  */
-public enum Bar implements BarIII {
+public enum Bar {
     B1(0),
     B2(0),
     B3(0);
--- src/a/b/c/BarIII.java
+++
@@ -1,7 +0,0 @@
-package a.b.c;
-
-/**
- * Created by irina on 6/10/2015.
- */
-public interface BarIII {
-}
